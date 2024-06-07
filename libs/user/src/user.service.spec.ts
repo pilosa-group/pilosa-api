dfc362e99017fe1671064fd7f8c3c55b2ee97e45
Edgar Post-Buijs <github@edgarpost.com>
2024-06-07 14:54:41 +0200

    fix tests

--- libs/user/src/user.service.spec.ts
+++ libs/user/src/user.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '@app/user/entities/user.entity';
+
+const mockUserRepository = {};
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
--- libs/web-metrics/src/beacon.controller.spec.ts
+++ libs/web-metrics/src/beacon.controller.spec.ts
@@ -1,5 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BeaconController } from './beacon.controller';
+import { BrowserMetric } from '@app/web-metrics/entities/browser-metric.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { FrontendApp } from '@app/web-metrics/entities/frontend-app.entity';
+import { FrontendAppService } from '@app/web-metrics/frontend-app.service';
+import { BrowserMetricService } from '@app/web-metrics/browser-metric.service';
+
+const mockBrowserMetricsRepository = {};
+const mockFrontendAppRepository = {};
 
 describe('BeaconController', () => {
   let controller: BeaconController;
@@ -7,6 +15,18 @@ describe('BeaconController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BeaconController],
+      providers: [
+        FrontendAppService,
+        BrowserMetricService,
+        {
+          provide: getRepositoryToken(BrowserMetric),
+          useValue: mockBrowserMetricsRepository,
+        },
+        {
+          provide: getRepositoryToken(FrontendApp),
+          useValue: mockFrontendAppRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<BeaconController>(BeaconController);
