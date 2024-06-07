46334994ce44fcfc741fe9a8cac95b3ffe03137e
Edgar Post-Buijs <github@edgarpost.com>
2024-06-07 13:43:38 +0200

    send more useful data in payload

--- apps/project-green-nest/src/web-snippet/web-snippet-injectable.ts
+++ apps/project-green-nest/src/web-snippet/web-snippet-injectable.ts
@@ -1,80 +1,228 @@
-let currentBytes = 0;
-let currentBytesCached = 0;
-let totalNumberOfEntries = 0;
-let numberOfEntriesWithBodySize = 0;
 const ENTRY_TYPE_RESOURCE = 'resource';
+
+type FullPageLoad = boolean;
+type Timestamp = number;
+type InitiatorType = string;
+type Domain = string;
+type Path = string;
+type Origin = string;
+type NumberOfBytes = number;
+type FileExtension = string;
+type CompressedBytes = NumberOfBytes;
+type UncompressedBytes = NumberOfBytes;
+
+type Payload = {
+  // The domain of the resource
+  domain: Domain;
+  // The path of the resource
+  path: Path;
+  // The type of the initiator of the resource
+  initiatorType: InitiatorType;
+  // Whether the resource is compressed
+  compressed: boolean;
+  // The current bytes of the resource
+  bytes: NumberOfBytes;
+  // The cross-origin origins of the resource, when it's a cross-origin request
+  crossOrigin: string[];
+  // extension
+  extension: FileExtension;
+};
+
+let payloads: Payload[] = [];
+
+type CombinedPayload = {
+  f: FullPageLoad;
+  t: Timestamp;
+  b: [CompressedBytes, UncompressedBytes];
+  d: {
+    [key: Domain]: {
+      [key: Path]: {
+        [key: InitiatorType]: {
+          [key: FileExtension]: {
+            b: [CompressedBytes, UncompressedBytes];
+            co: Origin[];
+          };
+        };
+      };
+    };
+  };
+};
 
 // These vars are injected by the endpoint
 declare let SNIPPET_API_ENDPOINT: string;
 declare let BATCH_REPORT_WAIT_TIME_IN_MS: number;
 
-const TYPE_RESOURCE = 'r';
 const noop = () => {};
 
 const CLIENT_ID = document.currentScript.getAttribute('data-client-id');
 
-let timeout: number | undefined;
+/**
+ * Debounce function
+ *
+ * @param fn
+ * @param wait
+ */
+const debounce = (fn: () => void, wait: number) => {
+  let timeout: number | undefined;
 
-const sendReport = () => {
-  if (timeout) {
+  return () => {
     clearTimeout(timeout);
+    timeout = window.setTimeout(fn, wait);
+  };
+};
+
+let fullPageLoad = true;
+
+const sendBeacon = (): void => {
+  const groupedPayloads: CombinedPayload = {
+    f: fullPageLoad,
+    t: Date.now(),
+    b: [0, 0],
+    d: {},
+  };
+
+  const compressedKey = 0;
+  const uncompressedKey = 1;
+
+  for (const payload of payloads) {
+    const domain = payload.domain;
+    const path = payload.path;
+    const initiatorType = payload.initiatorType;
+    const extension = payload.extension;
+    const compressed = payload.compressed;
+    const bytes = payload.bytes;
+    const crossOrigin = payload.crossOrigin;
+
+    // If the domain doesn't exist in the groupedPayloads, add it
+    if (!groupedPayloads.d[domain]) {
+      groupedPayloads.d[domain] = {};
+    }
+
+    // If the path doesn't exist in the groupedPayloads, add it
+    if (!groupedPayloads.d[domain][path]) {
+      groupedPayloads.d[domain][path] = {};
+    }
+
+    // If the initiatorType doesn't exist in the groupedPayloads, add it
+    if (!groupedPayloads.d[domain][path][initiatorType]) {
+      groupedPayloads.d[domain][path][initiatorType] = {};
+    }
+
+    // If the extension doesn't exist in the groupedPayloads, add it
+    if (!groupedPayloads.d[domain][path][initiatorType][extension]) {
+      groupedPayloads.d[domain][path][initiatorType][extension] = {
+        b: [0, 0],
+        co: [],
+      };
+    }
+
+    // Add the bytes to the groupedPayloads
+    groupedPayloads.d[domain][path][initiatorType][extension].b[
+      compressed ? compressedKey : uncompressedKey
+    ] += bytes;
+
+    // Add the cross-origin origins to the groupedPayloads
+    for (const origin of crossOrigin) {
+      if (
+        !groupedPayloads.d[domain][path][initiatorType][extension].co.includes(
+          origin,
+        )
+      ) {
+        groupedPayloads.d[domain][path][initiatorType][extension].co.push(
+          origin,
+        );
+      }
+    }
+
+    // Add the bytes to the total bytes
+    groupedPayloads.b[compressed ? compressedKey : uncompressedKey] += bytes;
   }
 
-  timeout = window.setTimeout(() => {
-    // TODO: accuracy based on the number of entries with body size
-    const accuracy = (numberOfEntriesWithBodySize / totalNumberOfEntries) * 100;
+  const hasBytes = groupedPayloads.b.some((bytes) => bytes > 0);
+  const totalBytes = groupedPayloads.b.reduce((acc, bytes) => acc + bytes, 0);
+  const hasData = Object.keys(groupedPayloads.d).length > 0;
 
-    const domain = window.location.hostname;
-    const path = window.location.pathname + window.location.search;
+  console.log('total MB', totalBytes / 1024 / 1024);
 
-    const payload = {
-      t: TYPE_RESOURCE,
-      b: currentBytes,
-      bc: currentBytesCached,
-      d: domain,
-      p: path,
-      a: accuracy,
-    };
-
-    clearTimeout(timeout);
-
+  if (hasBytes || hasData) {
+    console.log('send!', JSON.stringify(groupedPayloads, null, 2));
     fetch(SNIPPET_API_ENDPOINT, {
+      keepalive: true,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'x-id': CLIENT_ID,
       },
-      body: JSON.stringify(payload),
+      body: JSON.stringify(groupedPayloads),
     }).catch(noop);
+  }
 
-    // reset for next batch
-    currentBytes = 0;
-    currentBytesCached = 0;
-    numberOfEntriesWithBodySize = 0;
-    totalNumberOfEntries = 0;
-  }, BATCH_REPORT_WAIT_TIME_IN_MS);
+  // reset for next batch
+  payloads = [];
+  fullPageLoad = false;
 };
+
+// Send the beacon after a certain amount of time
+const sendBeaconDebounced = debounce(sendBeacon, BATCH_REPORT_WAIT_TIME_IN_MS);
 
 const observer = new PerformanceObserver((list) => {
   list.getEntries().map((entry: PerformanceResourceTiming) => {
+    const domain = window.location.hostname;
+    const path = window.location.pathname + window.location.search;
+
     switch (entry.entryType) {
       case ENTRY_TYPE_RESOURCE: {
-        currentBytes += entry.transferSize;
-        currentBytesCached += entry.encodedBodySize;
+        const cached = entry.transferSize === 0 && entry.decodedBodySize > 0;
 
-        if (entry.encodedBodySize > 0) {
-          numberOfEntriesWithBodySize++;
+        if (cached) {
+          return;
         }
 
-        totalNumberOfEntries++;
+        const url = new URL(entry.name);
+
+        // Check if the resource is compressed
+        const compressed =
+          entry.decodedBodySize &&
+          entry.decodedBodySize !== entry.encodedBodySize;
+
+        let extension = '__none__';
+        if (url.pathname.includes('.')) {
+          extension = url.pathname.split('.').pop();
+        }
+
+        const payload: Payload = {
+          domain,
+          path,
+          compressed,
+          initiatorType: entry.initiatorType,
+          bytes: entry.transferSize,
+          crossOrigin: [],
+          extension,
+        };
+
+        // If the transfer size is 0, it's a cross-origin request
+        if (entry.transferSize === 0) {
+          const origin = url.host;
+
+          // Only add the origin if it's not already in the list
+          if (!payload.crossOrigin.includes(origin)) {
+            payload.crossOrigin.push(origin);
+          }
+        }
+
+        payloads.push(payload);
+
         break;
       }
     }
   });
 
-  if (currentBytes > 0) {
-    sendReport();
+  // If there are payloads, send the beacon
+  if (payloads.length > 0) {
+    sendBeaconDebounced();
   }
 });
+
+document.addEventListener('visibilitychange', () => sendBeacon());
 
 observer.observe({ type: ENTRY_TYPE_RESOURCE, buffered: true });
