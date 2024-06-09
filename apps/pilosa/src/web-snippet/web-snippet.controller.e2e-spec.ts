0a743deb1d012156b8b0b867761df4e2e9312298
Edgar Post-Buijs <github@edgarpost.com>
2024-06-09 07:49:21 +0200

    add e2e test for web-snippet controller

---
+++ apps/pilosa/src/web-snippet/web-snippet.controller.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { WebSnippetController } from './web-snippet.controller';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+describe('WebSnippetController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [WebSnippetController],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET sloth.js', () => {
+    return request(app.getHttpServer())
+      .get('/sloth.js')
+      .expect(200)
+      .expect('Content-Type', 'text/javascript; charset=utf-8')
+      .expect('Cache-Control', 'public, max-age=60')
+      .expect('Timing-Allow-Origin', '*');
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+});
