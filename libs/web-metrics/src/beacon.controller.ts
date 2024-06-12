9c70e8f76ee94067ead7d9c148deebf521e5c738
Edgar Post-Buijs <github@edgarpost.com>
2024-06-12 13:25:41 +0200

    feat: check for frontend app id length

--- libs/web-metrics/src/beacon.controller.ts
+++ libs/web-metrics/src/beacon.controller.ts
@@ -134,6 +134,11 @@ export class BeaconController {
     @ClientIp() clientIp: string,
   ) {
     const frontendAppId = req.headers[FRONTEND_APP_ID] as Project['id'];
+
+    if (!frontendAppId || frontendAppId.length < 32) {
+      throw new BadRequestException('Invalid frontend app id');
+    }
+
     const frontendApp =
       await this.frontendAppService.findOneById(frontendAppId);
 
