a1644e16f54dbb8604f0643eef5bb89ab7123e20
Edgar Post-Buijs <github@edgarpost.com>
2024-06-25 16:57:12 +0200

    fix: don't break cron when scanning fails

--- libs/web-metrics/src/page-scanner.service.ts
+++ libs/web-metrics/src/page-scanner.service.ts
@@ -4,6 +4,7 @@ import { CreateRequestContext } from '@mikro-orm/core';
 import { MikroORM } from '@mikro-orm/core';
 import { BrowserMetricService } from '@app/web-metrics/browser-metric.service';
 import { SyntheticScanService } from '@app/synthetic-scan';
+import * as Sentry from '@sentry/node';
 
 @Injectable()
 export class PageScannerService {
@@ -28,9 +29,13 @@ export class PageScannerService {
 
     console.log('scanning', url);
 
-    await this.syntheticScanService.run(url, {
-      scrollToEnd: false,
-    });
+    try {
+      await this.syntheticScanService.run(url, {
+        scrollToEnd: false,
+      });
+    } catch (e) {
+      Sentry.captureException(e);
+    }
 
     this.isRunning = false;
   }
